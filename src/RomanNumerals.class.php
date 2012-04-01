c999f8280eb2a41e26262ded3db2a684cf05ffbf
Jan Krause <info@kraeuschen.de>
2012-04-01 22:08:31 +0200

    Added Kata Roman Numerals

---
+++ src/RomanNumerals.class.php
@@ -0,0 +1,69 @@
+<?php
+
+// +----------------------------------------------------------+
+// | phpCodeKatas Roman Numerals                              |
+// +----------------------------------------------------------+
+
+namespace phpCodeKatas;
+
+/**
+ * RomanNumbers.class.php
+ * 
+ * You should write a function to convert from normal numbers to Roman Numerals
+ * using letters - I, V, X, L, C, D, M
+ *
+ * @author j.krause <info@kraeuschen.de>
+ *
+ * @see http://www.novaroma.org/via_romana/numbers.html
+ * @see http://langrsoft.com/jeff/2011/09/tdd-kata-roman-number-converter/
+ * @see http://codingdojo.org/cgi-bin/wiki.pl?KataRomanNumerals
+ */
+class RomanNumerals
+{
+    /**
+     * Int to Char Mapper
+     * 
+     * @var array
+     */
+    private static $_map = array(1000 => 'M',
+                                 900  => 'CM',
+                                 500  => 'D',
+                                 400  => 'CD',
+                                 100  => 'C',
+                                 90   => 'XC',
+                                 50   => 'L',
+                                 40   => 'XL',
+                                 10   => 'X',
+                                 9    => 'IX',
+                                 5    => 'V',
+                                 4    => 'IV',
+                                 1    => 'I');
+    
+    /**
+     * converts int to roman
+     * 
+     * @param int $int
+     * 
+     * @return string
+     */
+    public static function convertToRoman($int) {
+        $roman = '';
+        $int = (int) $int;
+
+        if ($int <= 0) {
+            return $roman;
+        }
+
+        while ($int>0) {
+            foreach (self::$_map as $n => $char) {
+                if ($int >= $n) {
+                    $int -= $n;
+                    $roman .= $char;
+                    break;
+                }
+            }
+        }
+        
+        return $roman;
+    }
+}
---
+++ tests/RomanNumeralsTest.class.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once('../src//RomanNumerals.class.php');
+
+use phpCodeKatas\RomanNumerals;
+
+/**
+ * RomanNumeralsTest
+ *
+ * Unittests for the Roman Numerals Kata
+ *
+ * @author j.krause <info@kraeuschen.de>
+ */
+class RomanNumeralsTest extends PHPUnit_Framework_TestCase
+{
+    private $_dataProvider = array(array(0, ''),
+                                   array(1, 'I'),
+                                   array(2, 'II'),
+                                   array(3, 'III'),
+                                   array(4, 'IV'),
+                                   array(5, 'V'),
+                                   array(6, 'VI'),
+                                   array(7, 'VII'),
+                                   array(8, 'VIII'),
+                                   array(9, 'IX'),
+                                   array(10, 'X'),
+                                   array(11, 'XI'),
+                                   array(12, 'XII'),
+                                   array(13, 'XIII'),
+                                   array(14, 'XIV'),
+                                   array(15, 'XV'),
+                                   array(16, 'XVI'),
+                                   array(17, 'XVII'),
+                                   array(18, 'XVIII'),
+                                   array(19, 'XIX'),
+                                   array(20, 'XX'),
+                                   array(24, 'XXIV'),
+                                   array(25, 'XXV'),
+                                   array(40, 'XL'),
+                                   array(50, 'L'),
+                                   array(90, 'XC'),
+                                   array(100, 'C'),
+                                   array(400, 'CD'),
+                                   array(448, 'CDXLVIII'),
+                                   array(500, 'D'),
+                                   array(900, 'CM'),
+                                   array(1000, 'M'),
+                                   array(2012, 'MMXII'),
+                                   array(2499, 'MMCDXCIX'),
+                                   array(3949, 'MMMCMXLIX'));
+
+    /**
+     * data provider
+     * 
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return $this->_dataProvider;
+    }
+
+    /**
+     * @dataProvider dataProvider
+     * 
+     * @return void
+     */
+    public function testArabicToRomanTranslation($int, $roman){
+        $this->assertEquals($roman, RomanNumerals::convertToRoman($int));
+    }
+}
