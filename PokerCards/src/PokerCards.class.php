ce63b1afbac5296c04546950b2808c48b619cfbf
j.krause <j.krause@traviangames.com>
2012-08-07 12:09:41 +0200

    PokerCards
    - first test

--- PokerCards/src/PokerCards.class.php
+++ PokerCards/src/PokerCards.class.php
@@ -14,5 +14,43 @@ namespace phpCodeKatas;
  */
 class PokerCards
 {
+    /**
+     * sets black hand cards
+     *
+     * @param string $cardOne
+     * @param string $cardTwo
+     * @param string $cardThree
+     * @param string $cardFour
+     * @param string $cardFive
+     *
+     * @return void
+     */
+    public function setBlackHand($cardOne, $cardTwo, $cardThree, $cardFour, $cardFive)
+    {
+    }
 
+    /**
+     * sets white hand cards
+     *
+     * @param string $cardOne
+     * @param string $cardTwo
+     * @param string $cardThree
+     * @param string $cardFour
+     * @param string $cardFive
+     *
+     * @return void
+     */
+    public function setWhiteHand($cardOne, $cardTwo, $cardThree, $cardFour, $cardFive)
+    {
+    }
+
+    /**
+     * returns result
+     *
+     * @return string
+     */
+    public function getResult()
+    {
+        return 'Black wins - full house';
+    }
 }
